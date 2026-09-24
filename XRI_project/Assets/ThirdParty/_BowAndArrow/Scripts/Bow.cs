8a8bcbc8b373c4a86c190b0f0e77245914e05f5c
Jeremy Reynolds <jreynolds5756@gmail.com>
2026-09-24 10:19:18 -0400

    Bow Haptics

--- XRI_project/Assets/ThirdParty/_BowAndArrow/Scripts/Bow.cs
+++ XRI_project/Assets/ThirdParty/_BowAndArrow/Scripts/Bow.cs
@@ -1,5 +1,7 @@
-﻿using System.Collections;
+﻿using Oculus.Haptics;
+using System.Collections;
 using UnityEngine;
+using UnityEngine.XR.OpenXR.Input;
 
 public class Bow : MonoBehaviour
 {
@@ -19,6 +21,9 @@ public class Bow : MonoBehaviour
     private float m_PullValue = 0.0f;
 
     public float pullMultiplier = 1;
+
+    [Header("Haptics")]
+    public HapticClipPlayer hapticClipPlayer; //Reference to Meta Haptic Studio file
 
     private void Awake()
     {
@@ -91,6 +96,8 @@ public class Bow : MonoBehaviour
         // If we've pulled far enough, fire
         if (m_PullValue > 0.25f)
             FireArrow();
+        
+        PlayHapticFeedback();
 
         // Clear
         m_PullingHand = null;
@@ -108,5 +115,18 @@ public class Bow : MonoBehaviour
     {
         m_CurrentArrow.Fire(m_PullValue * pullMultiplier);
         m_CurrentArrow = null;
+    }
+
+    private void PlayHapticFeedback()
+    {
+        if (m_PullingHand != null)
+        {
+            HapticSource hapticSource = m_PullingHand.GetComponent<HapticSource>();
+
+            if (hapticSource != null)
+            {
+                hapticSource.Play();
+            }
+        }
     }
 }
